76f6ace6c983d6649dd78dfd45a5f3b62d77dae0
Minh200463 <kuidndjj@gmail.com>
2025-05-31 18:49:36 +0700

    ok1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'; // <- Nhớ import useEffect
 import 'aos/dist/aos.css';
 import './App.css';
 import Carousel from './component/wedding_index';
-import InvitationForm from './component/invitationForm';
+
 import AOS from 'aos';
 
 function App() {
